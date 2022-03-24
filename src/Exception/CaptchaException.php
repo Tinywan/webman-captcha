c84048267df1708c45687db6deae0a55a9aded07
万少波 <756684177@qq.com>
2022-03-24 21:52:30 +0800

    init

---
+++ src/Exception/CaptchaException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @desc CaptchaException.php 描述信息
+ * @author Tinywan(ShaoBo Wan)
+ * @date 2022/3/24 21:51
+ */
+declare(strict_types=1);
+
+
+namespace Tinywan\Captcha\Exception;
+
+
+class CaptchaException extends \RuntimeException
+{
+
+}
